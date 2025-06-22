34b89e10afcdc45cc3e25e05b0290be5ee187e2f
Breno Moura <281743@dac.unicamp.br>
2025-06-22 15:27:26 -0300

    guess-input component

--- frontend/unicample-frontend/src/app/shared/guess-input/guess-input.component.ts
+++ frontend/unicample-frontend/src/app/shared/guess-input/guess-input.component.ts
@@ -1,11 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-guess-input',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './guess-input.component.html',
-  styleUrl: './guess-input.component.scss'
+  styleUrls: ['./guess-input.component.scss']
 })
-export class GuessInputComponent {
 
+export class GuessInputComponent implements OnInit {
+
+  @Output() guessSubmitted = new EventEmitter<string>();
+
+  isPanelOpen = false;
+  selectedValue: string | null = null;
+  filterText: string = '';
+
+  //puxar do banco de dados
+  private allLocations: string[] = [
+    "Instituto de Física Gleb Wataghin",
+    "Praça Do Ciclo Básico",
+    "Biblioteca Cesar Lattes",
+    "Ciclo Básico I",
+    "Ciclo Básico II",
+    "Biblioteca de Obras Raras",
+    "Faculdade de Educação",
+    "Faculdade de Engenharia Agrícola",
+    "Faculdade de Engenharia Civil Arquitetura e Urbanismo",
+    "Faculdade de Educação Física",
+    "Faculdade de Engenharia Mecânica",
+    "Instituto de Artes",
+    "Instituto da Biologia",
+    "Instituto da Computação",
+    "Instituto da Economia",
+    "Instituto de Estudo Da Linguagem",
+    "Instituto de Filosofia e Ciências Humanas",
+    "Instituto de Geociências",
+    "Instituto de Matemática Estatística e Computação Científica",
+    "Instituto de Quimica",
+    "Plasma",
+    "Restaurante Universitário"
+  ];
+
+  filteredLocations: string[] = [];
+
+  ngOnInit(): void {
+    this.filteredLocations = [...this.allLocations];
+  }
+
+  openPanel(): void {
+    this.isPanelOpen = true;
+  }
+
+  closePanel(): void {
+    this.isPanelOpen = false;
+  }
+
+  selectLocation(location: string): void {
+    this.selectedValue = location;
+    this.filterText = location; 
+    this.filterLocations();
+  }
+
+  submitGuess(): void {
+    if (this.selectedValue) {
+      this.guessSubmitted.emit(this.selectedValue);
+      this.closePanel();
+    } else {
+      alert("Por favor, selecione um local da lista.");
+    }
+  }
+
+  filterLocations(): void {
+    const text = this.filterText.toLowerCase();
+    this.filteredLocations = this.allLocations.filter(location =>
+      location.toLowerCase().includes(text)
+    );
+  }
 }
